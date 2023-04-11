f5c863d6a6763513895b024ce558ca5c8cd47ff3
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-04-11 11:23:25 +0300

    Add files via upload

---
+++ Task2/main.cpp
@@ -0,0 +1,16 @@
+//Write a function that returns the cube of a number
+
+#include <iostream>
+
+using namespace std;
+
+int Kub(int a)
+{
+    return a * 3;
+}
+
+int main()
+{
+    int result = Kub(5);
+    cout << "Kubes num : " << result << endl;
+}
